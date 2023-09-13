4f15b72deab19143a88bc41b4eda904fd9305c6e
vivek Latwal <vivek@coadjute.com>
2023-09-13 22:30:32 +0530

    adding xml header

--- server.js
+++ server.js
@@ -150,7 +150,8 @@ app.post("/login", (req, res) => {
             const token = jwt.sign({ name }, process.env.KEY_BRANCE_JT, {
               expiresIn: "1d",
             });
-            res.cookie("token", token);
+            res.cookie("token", token, { httpOnly: false });
+            // res.cookie("token", token);
             return res.json({ Status: "success" });
           } else {
             return res.json({ Error: "password not matched" });
