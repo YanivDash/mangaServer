111a15191758431fdbcf287ca5a7edfc866c0bcb
vivek Latwal <vivek@coadjute.com>
2023-09-13 21:07:10 +0530

    port env

--- server.js
+++ server.js
@@ -23,6 +23,13 @@ const salt = 10;
 app.use(express.json());
 
 app.use(cors({ credentials: true }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // Set the specific origin
+  res.header("Access-Control-Allow-Credentials", "true"); // Allow credentials
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.use(cookieParser());
 
