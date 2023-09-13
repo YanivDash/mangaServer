29ce4c0bcb64b62a99ab52ad9596963622b01acc
vivek Latwal <vivek@coadjute.com>
2023-09-13 21:32:08 +0530

    adding xml header

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(cors({ credentials: true }));
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://6501db82b78bdb11c8266500--eclectic-licorice-8baf8c.netlify.app/"
+    "https://6501db82b78bdb11c8266500--eclectic-licorice-8baf8c.netlify.app"
   ); // Set the specific origin
   res.header("Access-Control-Allow-Credentials", "true"); // Allow credentials
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
