5bf7e12e6fd2390af607ed4d4a6e8e6ea3590364
vivek Latwal <vivek@coadjute.com>
2023-09-18 14:28:21 +0530

    soving connection error

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -2,44 +2,81 @@ import mysql from "mysql";
 import dotenv from "dotenv";
 dotenv.config();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
+let db;
 
-db.connect(function (err) {
-  if (err) {
-    console.log("error when connecting to db:", err);
-    setTimeout(function () {
-      db.connect(function (err) {
-        if (err) {
-          console.log("Error while reconnecting:", err);
-        } else {
-          console.log("Reconnected to the database");
-        }
-      });
-    }, 2000);
-  } else {
-    console.log("Connected to the database");
-  }
-});
+function handleDisconnect() {
+  db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  });
 
-db.on("error", function (err) {
-  console.log("db error", err);
-  if (err.code === "PROTOCOL_CONNECTION_LOST") {
-    // Attempt to reconnect
-    db.connect(function (err) {
-      if (err) {
-        console.log("Error while reconnecting:", err);
-      } else {
-        console.log("Reconnected to the database");
-      }
-    });
-  } else {
-    throw err;
-  }
-});
+  db.connect(function (err) {
+    if (err) {
+      console.log("error when connecting to db:", err);
+      setTimeout(handleDisconnect, 2000);
+    } else {
+      console.log("Connected to the database");
+    }
+  });
+
+  db.on("error", function (err) {
+    console.log("db error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      console.log("Reconnecting to the database...");
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 export default db;
+
+// import mysql from "mysql";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+// });
+
+// db.connect(function (err) {
+//   if (err) {
+//     console.log("error when connecting to db:", err);
+//     setTimeout(function () {
+//       db.connect(function (err) {
+//         if (err) {
+//           console.log("Error while reconnecting:", err);
+//         } else {
+//           console.log("Reconnected to the database");
+//         }
+//       });
+//     }, 2000);
+//   } else {
+//     console.log("Connected to the database");
+//   }
+// });
+
+// db.on("error", function (err) {
+//   console.log("db error", err);
+//   if (err.code === "PROTOCOL_CONNECTION_LOST") {
+//     // Attempt to reconnect
+//     db.connect(function (err) {
+//       if (err) {
+//         console.log("Error while reconnecting:", err);
+//       } else {
+//         console.log("Reconnected to the database");
+//       }
+//     });
+//   } else {
+//     throw err;
+//   }
+// });
+// export default db;
