cd95ada2fa03a8aafa878ae9a7ce38405e26a329
vivek Latwal <vivek@coadjute.com>
2023-09-18 14:46:54 +0530

    soving connection error

--- controlers/getAllManga.js
+++ controlers/getAllManga.js
@@ -3,10 +3,10 @@ import db from "../database/dbConnection.js";
 const getAllManga = () => {
   return new Promise((resolve, reject) => {
     const sql = `SELECT * FROM mangalist`;
-    db.query(sql, (err, result) => {
-      if (err) {
-        console.error(err);
-        reject(err);
+    db.query(sql, (error, result) => {
+      if (error) {
+        console.error("Error executing the query:", error);
+        reject(error);
       } else {
         resolve(result);
       }
