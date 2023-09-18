e842893d3e161c747ba73c75e0007bb2a220b427
vivek Latwal <vivek@coadjute.com>
2023-09-18 13:03:51 +0530

    db server handle error

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -15,11 +15,11 @@ db.connect(function (err) {
     setTimeout(db, 2000);
   }
 });
-
+console.log("error is here");
 db.on("error", function (err) {
   console.log("db error", err);
   if (err.code === "PROTOCOL_CONNECTION_LOST") {
-    db();
+    db;
   } else {
     throw err;
   }
