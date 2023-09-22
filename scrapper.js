579346d6eaa0e032d60025a56c97b89481fa5033
vivek Latwal <vivek@coadjute.com>
2023-09-22 23:26:12 +0530

    scrape advane

--- scrapper.js
+++ scrapper.js
@@ -5,10 +5,8 @@ const scraper = async (url, elemClass) => {
   let data = [];
   let currentIndex = 0;
 
-  let imgType = ["jpg", "jpeg", "chapter", "png"];
-  for (let i = imgType.length - 1; i >= 0; i--) {
-    imgType[i + 1] = imgType[i];
-  }
+  let imgType = ["", "jpg", "jpeg", "chapter", "png"];
+
   imgType[0] = elemClass;
   imgType = Array.from(new Set(imgType));
 
@@ -19,21 +17,19 @@ const scraper = async (url, elemClass) => {
       searchRegex = `img`;
     }
 
-    const response = await axios.get(url);
-    const $ = cheerio.load(response.data);
-
     try {
+      const response = await axios.get(url);
+      const $ = cheerio.load(response.data);
       const images = $(searchRegex);
       images.each(async (index, element) => {
         const imageUrl = $(element).attr("src");
-        // console.log(imageUrl);
+
         let cleanedLink = imageUrl.replace(/\t/g, "").replace(/\n/g, "");
         data.push(cleanedLink);
       });
 
       if (data.length > 4) {
         console.log(`elemments with class ${searchClass}`);
-        console.log(data);
       } else {
         console.log(
           `No img elemments with class ${searchClass} found on the page.`
@@ -100,7 +96,6 @@ const scrapeTotal = async (url) => {
 
 const scrapeLinks = async (url) => {
   const elemClass = "a[href*=chapter]";
-  // url = "https://tomodachimanga.com/";
 
   let data = [];
 
@@ -113,38 +108,19 @@ const scrapeLinks = async (url) => {
       data.push(link);
     });
 
-    let firstTen = data.slice(0, 10);
-    let lastTen = data.slice(-10);
-
     data = Array.from(new Set(data));
 
-    let sortedFirstTen = data.slice(0, 20);
-    let sortedLastTen = data.slice(-20);
-
-    firstTen.forEach((item) => {
-      if (!item in sortedFirstTen && !item in sortedLastTen) {
-        data.append(item);
-      }
-    });
-
-    lastTen.forEach((item) => {
-      if (!item in sortedFirstTen && !item in sortedLastTen) {
-        data.push(item);
-      }
-    });
-
     if (data.length > 3) {
-      console.log(data);
       return data;
     } else {
       return "failed to load chapters";
     }
   } catch (error) {
-    console.log("error");
+    console.log(error);
     console.log(url);
   }
 };
-
+scrapeLinks();
 const updateChapter = async (url) => {
   const elemClass = "a[href*=chapter]";
   let data = [];
@@ -180,74 +156,3 @@ const updateChapter = async (url) => {
   }
 };
 export { scraper, scrapeTotal, scrapeLinks, updateChapter };
-
-// const scraper = async (url, chapClass) => {
-//   const data = [];
-
-//   const response = await axios.get(url);
-//   const $ = cheerio.load(response.data);
-//   const images = $(chapClass);
-
-//   images.each(async (index, element) => {
-//     const imageUrl = $(element).attr("src");
-//     let cleanedLink = imageUrl.replace(/\t/g, "").replace(/\n/g, "");
-//     data.push(cleanedLink);
-
-//     // Skip if the src attribute is missing or if it's a data URI
-//     if (!imageUrl || imageUrl.startsWith("data:")) {
-//       return;
-//     }
-//   });
-//   return data;
-// };
-
-// const scrapTotal = async (mangaLink, mangaClass) => {
-//   let totalChapter = 0;
-//   const regex = /chapterNumberHere/;
-//   let typeUrl = mangaLink.replace(regex, `8010268116`);
-//   const typeRegex = /8010268116/;
-
-//   let leap = 1000;
-//   let newUrl = mangaLink.replace(regex, `${1}`);
-//   let match = 1;
-//   let imageUrl;
-
-//   while (true) {
-//     try {
-//       const response = await axios.get(newUrl);
-//       const $ = cheerio.load(response.data);
-//       const images = $(mangaClass);
-//       imageUrl = $(images[0]).attr("src");
-//     } catch (error) {
-//       console.log({ match: "error" });
-//       imageUrl = "";
-//     }
-//     let numNow = new RegExp(`${match}`);
-//     if (imageUrl) {
-//       newUrl = typeUrl.replace(typeRegex, `${match + leap}`);
-//       match = match + leap;
-//     } else if (leap === 1000) {
-//       newUrl = typeUrl.replace(typeRegex, `${match - 1000}`);
-//       match = match - 1000;
-//       leap = 500;
-//     } else if (leap === 500) {
-//       newUrl = typeUrl.replace(typeRegex, `${match - 500}`);
-//       leap = 100;
-//       match = match - 500;
-//     } else if (leap === 100) {
-//       newUrl = typeUrl.replace(typeRegex, `${match - 100}`);
-//       leap = 10;
-//       match = match - 100;
-//     } else if (leap === 10) {
-//       newUrl = typeUrl.replace(typeRegex, `${match - 10}`);
-//       leap = 1;
-//       match = match - 10;
-//     } else {
-//       totalChapter = match - 1;
-//       break;
-//     }
-//   }
-
-//   console.log({ total: totalChapter });
-//   return totalChapter;
-// };
