22752d99fdc37ff3e471e4a31712d6508a0374a9
vivek Latwal <vivek@coadjute.com>
2023-09-28 22:06:26 +0530

    adde delete manga

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 app.use(
   cors({
     origin: ["https://manganexus-library.netlify.app"],
-    methods: ["POST", "GET"],
+    methods: ["POST", "GET", "DELETE"],
     credentials: true,
   })
 );
